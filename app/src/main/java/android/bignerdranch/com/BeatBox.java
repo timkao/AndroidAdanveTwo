71d7a494e60c4b7f0dc9590ee244de83a7de2949
Tim Kao <pakuya.kao@gmail.com>
2019-12-29 12:09:55 -0800

    getting assets with context getAssets

---
+++ app/src/main/java/android/bignerdranch/com/BeatBox.java
@@ -0,0 +1,29 @@
+package android.bignerdranch.com;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
+
+public class BeatBox {
+    private static final String TAG = "BeatBox";
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+    private AssetManager mAssets;
+
+    public BeatBox(Context context) {
+        mAssets = context.getAssets();
+        loadSounds();
+    }
+
+    private void loadSounds() {
+        String[] soundNames;
+        try {
+            soundNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found " + soundNames.length + " sounds");
+        } catch (IOException ioe) {
+            Log.e(TAG, "could not list assests");
+            return;
+        }
+    }
+}
--- app/src/main/java/android/bignerdranch/com/BeatBoxFragment.java
+++ app/src/main/java/android/bignerdranch/com/BeatBoxFragment.java
@@ -16,8 +16,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 public class BeatBoxFragment extends Fragment {
+
+    private BeatBox mBeatBox;
+
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBeatBox = new BeatBox(getActivity());
     }
 
     @Nullable
